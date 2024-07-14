cdd819becb65dc66e7c1e405acce91434eff2946
Han-Elliot Phan <hanelliotphan@gmail.com>
2024-07-13 20:43:35 -0700

    Initial commit

---
+++ get-data-by-player/src/utils/get-data.py
@@ -0,0 +1,13 @@
+"""
+get-data.py
+
+Get data by League of Legends player (either PUUID or Player name + game tag), 
+generate a CSV file and store the dataset in the 
+`./get-data-by-player/datasets` folder.
+--------------------------------------------------------------------------------
+Author(s):
+
+Name: Han-Elliot Phan
+Email: hanelliotphan@gmail.com
+Last updated at: July 13, 2024
+"""
